762565f83c999c3f450504bc113677ced68c3a04
Yash Adulkar <yash151099@gmail.com>
2021-05-29 18:57:25 +0530

    Update dashboard.dart

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:coronavirus_rest_api_app/app/repositories/data_repositories.dart';
 import 'package:coronavirus_rest_api_app/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_app/app/services/api.dart';
 import 'package:coronavirus_rest_api_app/app/ui/endpoint_card.dart';
+import 'package:coronavirus_rest_api_app/app/ui/last_updated_status_text.dart';
+import 'package:coronavirus_rest_api_app/app/ui/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,19 +17,40 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   EndpointsData? _endpointsData;
   Future<void> _updateData() async {
-    final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final endpointsData = await dataRepository.getAllEndpointsData();
-    setState(() => _endpointsData = endpointsData);
+    try {
+      final dataRepository =
+          Provider.of<DataRepository>(context, listen: false);
+      final endpointsData = await dataRepository.getAllEndpointsData();
+      setState(() => _endpointsData = endpointsData);
+    } on SocketException catch (_) {
+      showAlertDialog(
+        context: context,
+        title: "Connection Error",
+        content: "Could not retreive data. Please try again later",
+        defaultActionText: "Ok",
+      );
+    } catch (_) {
+      showAlertDialog(
+        context: context,
+        title: "Unknown Error",
+        content: "Please contact support or try again later",
+        defaultActionText: "Ok",
+      );
+    }
   }
 
   @override
   void initState() {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    _endpointsData = dataRepository.getAllEndpointsCachedData();
     _updateData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    final formatter = LastUpdatedDateFormatter(
+        lastUpdated: _endpointsData?.values[Endpoint.cases]?.date);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -36,10 +61,13 @@ class _DashboardState extends State<Dashboard> {
         onRefresh: _updateData,
         child: ListView(
           children: <Widget>[
-            for (var endpoint in Endpoints.values)
+            LastUpdatedStatusText(
+              text: formatter.lastUpdatedStatusText(),
+            ),
+            for (var endpoint in Endpoint.values)
               EndpointCard(
                 endpoints: endpoint,
-                value: _endpointsData?.values[endpoint] ?? null,
+                value: _endpointsData?.values[endpoint]?.value,
               ),
           ],
         ),
